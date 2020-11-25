ae116707117038ec84d857cd7c1f2cb267cab256
c8wu <c8wu@c8wu-11968.pods.c8wu.svc.cluster.local>
2020-11-25 05:32:05 -0800

    update on run.py, src/data/make_datasets.py, and .gitignore

--- run.py
+++ run.py
@@ -1,25 +1,11 @@
 import os
 import json
-config_path = "./config/config.json"
+# data module is used to retrieve data and process it using the pipeline
+import src.data.make_dataset as dm
 
-# retrive the data according to config/config
-def data_retrieve():
-    data_dir = config["data"][0]
-    datasets = os.listdir(data_dir)
-    datasets.sort()
-    reference = datasets[-3:]
-    datasets = datasets[:-3]
-    result = []
-    for i in range(0, len(datasets), 2):
-        result.append([os.path.join(data_dir, datasets[i]), os.path.join(data_dir, datasets[i + 1])])
-    for i in range(len(reference)):
-        reference[i] = os.path.join(data_dir, reference[i])
-    return result, reference
-
+def main():
+    return
 
 if __name__ == "__main__":
-    # get the config file
-    with open(config_path) as f:
-        config = json.load(f)
-    datas, reference = data_retrieve()
-    print(datas[0], reference)
+    main()
+    
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,29 +2,106 @@
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import json
+import os
+
+# get the config file
+with open("./config/config.json") as f:
+    config = json.load(f)
+
+# set the output directory
+fastq_output_dir = config["processed"]["fastq"]
+fastqc_dir = config["tools"]["FastQC"]
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+# @click.command()
+# @click.argument('input_filepath', type=click.Path(exists=True))
+# @click.argument('output_filepath', type=click.Path())
+# def main(input_filepath, output_filepath):
+#     """ Runs data processing scripts to turn raw data from (../raw) into
+#         cleaned data ready to be analyzed (saved in ../processed).
+#     """
+#     logger = logging.getLogger(__name__)
+#     logger.info('making final data set from raw data')
+
+# the function to retrieve the path for the datasets
+def data_retrieve():
+    data_dir = config["data"][0]
+    datasets = os.listdir(data_dir)
+    datasets.sort()
+    reference = datasets[-3:]
+    datasets = datasets[:-3]
+    result = []
+    for i in range(0, len(datasets), 2):
+        result.append([os.path.join(data_dir, datasets[i]), os.path.join(data_dir, datasets[i + 1])])
+    for i in range(len(reference)):
+        reference[i] = os.path.join(data_dir, reference[i])
+    return result, reference
+
+# quality control the datasets using the FastQC
+def run_fastqc(fastq_path, output_dir):
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    Run the fastqc program on a specified fastq file and return the output directory path.
+
+    Parameters
+    ----------
+    fastq_path : str
+        Path to the fastq file to analyze.
+    output_dir : str
+        Directory where output will be written. It will be created if it does not exist.
+    options : list of str (optional)
+        Command-line flags and options to fastqc. Default is --extract.
+
+    Returns
+    -------
+    output_dir : str
+        The path to the directory containing the detailed output for this fastq file.
+        It will be a subdirectory of the specified output dir.
+    """
+    utils.mkdir_p(output_dir)
+
+    command = "fastqc {} -o {} {}".format(' '.join(options), output_dir, fastq_path)
+    subprocess.check_call(command, shell=True)
+
+    # Fastqc creates a directory derived from the basename
+    fastq_dir = os.path.basename(fastq_path)
+    if fastq_dir.endswith(".gz"):
+        fastq_dir = fastq_dir[0:-3]
+    if fastq_dir.endswith(".fq"):
+        fastq_dir = fastq_dir[0:-3]
+    if fastq_dir.endswith(".fastq"):
+        fastq_dir = fastq_dir[0:-6]
+    fastq_dir = fastq_dir + "_fastqc"
+
+    # Delete the zip file and keep the uncompressed directory
+    zip_file = os.path.join(output_dir, fastq_dir + ".zip")
+    os.remove(zip_file)
+
+    output_dir = os.path.join(output_dir, fastq_dir)
+    return output_dir
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+# main function
+def main():
+    datas, reference = data_retrieve()
+    sample = datas[0]
+    print(sample)
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+if __name__ == "__main__":
 
+    print("Executing make_dataset.py on command line")
+
+elif __name__ == 'src.data.make_dataset':
+    # log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    # logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # # not used in this stub but often useful for finding various files
+    # project_dir = Path(__file__).resolve().parents[2]
+
+    # # find .env automagically by walking up directories until it's found, then
+    # # load up the .env entries as environment variables
+    # load_dotenv(find_dotenv())
+    print("src/data/make_dataset.py is imported, and data is being processed")
     main()
+    print("data processing is done")
