26339f20451c00f9aab0850aa6da417fbb8a4d20
denncc <denn_c@outlook.com>
2020-12-09 10:58:47 -0800

    finished input matrix for DESeq2

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -10,6 +10,8 @@ with open("./config/feature_config.json") as f:
 r_scipt_dir = feature_config["r_script_dir"]
 # Kallisto out dir
 kallisto_out_dir = feature_config["processed"]["kallisto"]
+# dir for DESeq2 output dir
+deseq_cts_matrix_dir = feature_config["features"]["deseq_cts_matrix_dir"]
 
 def test_r():
     """
@@ -23,8 +25,17 @@ def make_cts():
     This method doesn't have an input, but rather takes 352 abundance.tsv in the processed
     Kallisto directory, and makes a matrix that counts different subfeatures.
     """
-    abundances_dir = os.listdir(kallisto_out_dir)
-    abundances_dir.sort()
-    print(os.listdir(kallisto_out_dir))
-    print(abundances_dir)
+    abundances_dirs = os.listdir(kallisto_out_dir)
+    abundances_dirs.sort()
+    # cols_name = pd.read_csv(os.path.join(kallisto_out_dir, abundances_dirs[0], "abundance.tsv"), sep="\t").target_id
+    # print(cols_name)
+    result = pd.DataFrame()
+    for pair in abundances_dirs:
+        abundances_dir = os.path.join(kallisto_out_dir, pair, "abundance.tsv")
+        df = pd.read_csv(abundances_dir, sep="\t")
+        df = df.set_index("target_id")
+        est_counts = df.est_counts
+        result[pair] = est_counts
+    result.to_csv(deseq_cts_matrix_dir, sep="\t")
+    # print(abundances_dir)
     return
--- src/features/r_scripts/test.R
+++ src/features/r_scripts/test.R
@@ -1,3 +1,103 @@
+# code pulled from vignette: http://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html#quick-start
+
+# install packages
 if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-BiocManager::install()
+  install.packages("BiocManager")
+BiocManager::install("DESeq2")
+BiocManager::install("pasilla")
+BiocManager::install("apeglm")
+
+# ---------------- IMPORTS ---------------- #
+
+# import packages
+library("pasilla")
+library("DESeq2")
+
+# ---------------- DATA ---------------- #
+
+# get data
+pasCts <- system.file("extdata",
+                      "pasilla_gene_counts.tsv",
+                      package="pasilla", mustWork=TRUE)
+pasAnno <- system.file("extdata",
+                       "pasilla_sample_annotation.csv",
+                       package="pasilla", mustWork=TRUE)
+print(pasAnno)
+cts <- as.matrix(read.csv(pasCts,sep="\t",row.names="gene_id"))
+coldata <- read.csv(pasAnno, row.names=1)
+coldata <- coldata[,c("condition","type")]
+coldata$condition <- factor(coldata$condition)
+coldata$type <- factor(coldata$type)
+
+# our data
+temp <- as.matrix(read.csv("./data/SraRunTable.csv"))
+our_cts <- as.matrix(read.csv("./data/deseq_cts.tsv", sep="\t"), row.names("target_id"))
+
+# look at the data
+head(cts,2)
+coldata
+
+# not in same order! 
+rownames(coldata) <- sub("fb", "", rownames(coldata))
+
+# the same samples
+all(rownames(coldata) %in% colnames(cts))
+# but not the same order!
+all(rownames(coldata) == colnames(cts))
+
+# sort to be in the same order
+cts <- cts[, rownames(coldata)]
+all(rownames(coldata) == colnames(cts))
+
+# ---------------- DESeqDataSet ---------------- #
+
+# create DESeqDataSet object
+dds <- DESeqDataSetFromMatrix(countData = cts,
+                              colData = coldata,
+                              design = ~ condition)
+dds
+
+# set up metadata
+featureData <- data.frame(gene=rownames(cts))
+mcols(dds) <- DataFrame(mcols(dds), featureData)
+mcols(dds)
+
+# ---------------- FILTERING ---------------- #
+
+# pre-filter
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep,]
+
+# factors in R!
+dds$condition <- factor(dds$condition, levels = c("untreated","treated"))
+
+# ---------------- DEA ---------------- #
+
+# Differential expression analysis
+?DESeq
+# carries out: estimation of size factors, estimation of dispersion: neg. binomial GLM
+dds <- DESeq(dds)
+res <- results(dds)
+res
+
+# Log fold change
+resLFC <- lfcShrink(dds, coef="condition_treated_vs_untreated", type="apeglm")
+resLFC
+
+resOrdered <- resLFC[order(resLFC$pvalue),]
+sum(resOrdered$padj < 0.1, na.rm=TRUE)
+
+res05 <- results(dds, alpha=0.05)
+summary(res05)
+
+# ---------------- PAPER ---------------- #
+
+# the paper approach:
+
+# LRT
+dds <- DESeq(dds, test="LRT", reduced=~1)
+res <- results(dds)
+
+## variance stabilizing
+vsd <- vst(dds, blind=FALSE)
+
